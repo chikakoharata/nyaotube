f0280d4651d7d7a427bda2c6bfd621e2a6fb4d88
harata <c.oki1216@gmail.com>
2020-03-20 17:42:15 +0900

    ログアウト機能の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_082225) do
+ActiveRecord::Schema.define(version: 2020_03_20_075322) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
@@ -19,9 +19,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_082225) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
