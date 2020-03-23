7ca7c98c15d36e120f4fb6f55fa0afb5d06b738d
harata <c.oki1216@gmail.com>
2020-03-23 16:49:32 +0900

    正常なユーザー情報の更新

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Nyaotube
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    I18n.config.locales = :ja
-    I18n.default_locale = :ja
+    config.i18n.default_locale = :ja
   end
 end
